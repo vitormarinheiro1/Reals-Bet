ef7b3a205e92f6187e8423ab953715cc0a659310
vitormarinheiro1 <vitormarinheiro9@gmail.com>
2024-11-27 13:26:03 -0300

    feat: add migration de afiliados

---
+++ database/migrations/2024_11_27_162503_create_affiliates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('affiliates', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('cpf')->unique();
+            $table->date('birth_date');
+            $table->string('email')->unique();
+            $table->string('phone');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->boolean('active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('affiliates');
+    }
+};

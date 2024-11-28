a93dcfa55aad6847680ba19ae0b0993de19d0e65
Vitor <vitormarinheiro9@gmail.com>
2024-11-27 23:03:42 -0300

    fix: nao loga se usuario estiver inativo

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -14,8 +15,15 @@ class LoginController extends Controller
 
     public function store(Request $request)
     {
+        $user = User::where('email', $request->email)->first();
+        
+        if ($user && !$user->active) {
+            return redirect()->back()->withErrors(['email' => 'Este usuário está inativo.']);
+        }
+
+        // Se o usuário estiver ativo, tente autenticar
         if (!Auth::attempt($request->only('email', 'password'))) {
-            return redirect()->back()->withErrors(['Usuários ou senha inválidos']);
+            return redirect()->back()->withErrors(['email' => 'Usuário ou senha inválidos.']);
         }
         return to_route('users.index');
     }
